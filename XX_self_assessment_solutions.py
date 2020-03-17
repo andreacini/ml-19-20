0506f225275270f31a8d1607e82ff5a17393c8db
Daniele Zambon <daniele.zambon@usi.ch>
2020-03-17 16:16:18 +0100

    added solutions to XX_self_assessments Ex0

---
+++ XX_self_assessment_solutions.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+# 1.
+A = np.random.randn(5, 9) * 0.2 + 3.5 
+print("shape of A:", A.shape)  # This should be (5, 9)
+# 2.
+b = A.reshape(-1)
+print("shape of b:", b.shape)  # This should be equal to 49 = 5*9 
+b_mean = b.mean()
+print("mean of b:", b_mean)  # This should be approximately 1.5
+b_var = b.var()
+print("var of b:", b_var)  # This should be approximately 0.04 = 0.2**2 
+# 3.
+C = A.dot(A.T)
+# 4.
+C_diag = np.diag(C)
+print("diag of C:", C_diag) # They should all be roughly in between 100 and 120
+# 5.
+C_tr = C_diag.sum()
+print("trace of C:", C_tr)
+# 6.
+C_det = np.linalg.det(C)
+print("det of C:", C_det)
+# 7.
+eigval, eigvec = np.linalg.eigh(C)
+lam = eigval
+# 8. it should be equal to C_tr
+print("sum eigval:", lam.sum())
+# 9. it should be equal to C_det
+print("prod eigval:", lam.prod())
+# 10.
+U = eigvec
+# 11.
+L = np.diag(lam)
+D = U.dot(L).dot(U.T)
+print("check D==C:", np.all(D == C))  # It may be not true, due to small approximation errors
+print("check D approximately C:", np.allclose(D, C))  # This one is more appropriate to compare float numbers
+
